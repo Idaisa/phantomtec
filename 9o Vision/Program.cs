0c8e1379c731fcbc5ef20a622520fe39aabce2de
whosdatdev <whosdatdev@gmail.com>
2017-07-22 00:23:02 +0200

    Initial version, ward tracker

---
+++ 9o Vision/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9o_Vision
+{
+    using Aimtec.SDK.Events;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvents.GameStart += new WardTracker().OnLoad;
+        }
+    }
+}
